756f0d180c1d13d0a80643270d945bcb8339561c
Isnard Silva <isnardsilva@icloud.com>
2022-02-18 23:32:34 -0300

    Add VariableAndConstants playgrounds

---
+++ Foundation/VariableAndConstants.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+// Variáveis podem mudar o seu valor conforme necessário
+var name = "Ted"
+name = "Rebecca"
+
+// Constantes não podem ter o seu valor alterado após a sua inicialização
+let user = "Daphne"
+print(user)
+
