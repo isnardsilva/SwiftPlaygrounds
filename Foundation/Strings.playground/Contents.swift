a71ed9ee102e03769f8e2589d6582f88fd42331f
Isnard Silva <isnardsilva@icloud.com>
2022-02-18 23:53:05 -0300

    Add Strings.playground

---
+++ Foundation/Strings.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// Pode-se usar emojis
+let actor = "Tom Cruise 🏃‍♂️"
+
+
+// Como utilizar caracteres reservados
+let quote = "He tapped a sign saying \"Believe\" and walked away."
+
+
+// String com múltiplas linhas
+let movie = """
+A day in
+the life of an
+Apple enginer
+"""
+
+// Contador de caracteres
+print(actor.count)
+
+
+// Prefixo e Sufixo
+print(quote.hasPrefix("He"))
+print(quote.hasSuffix("Away."))
